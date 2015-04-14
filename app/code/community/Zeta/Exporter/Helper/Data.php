cd8a0aa56926fa80c14c889774625a6bacb09635
Gianluca <gianluca.zucco3@gmail.com>
2015-04-14 09:50:41 +0200

    Created base magento directory structure

---
+++ app/code/community/Zeta/Exporter/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ${Gianluca}
+ * Date: 14/04/15
+ * Time: 9.47
+ * To change this template use File | Settings | File Templates.
+ */ 
+class Zeta_Exporter_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
